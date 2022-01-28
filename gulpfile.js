e9070c571195f9ba2d2b79e6ec00ba5fec52bde4
Krada88 <studio-orange@yandex.ru>
2022-01-28 16:27:23 +0700

    fix gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -6,10 +6,3 @@ exports.clean = require('./gulp-tasks/clean');
 exports.html = require('./gulp-tasks/html');
 exports.serve = require('./gulp-tasks/serve');
 exports.styles = require('./gulp-tasks/styles');
-
-// gulp.task('watch', () => {
-//     notify('проверка')
-//     gulp.watch('./dev/**/*.less', gulp.parallel(['styles']))
-// }) 
-
-//gulp.task('default', gulp.series(['clean', 'server', 'watch']));
